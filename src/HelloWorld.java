54321249ba98ea22fa355258f43c756cae742bec
Darshan.N <darshan.n@clarivate.com>
2023-02-06 19:21:14 +0530

    testTwo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,30 @@
 public class HelloWorld {
+
+   public String myname = "Raj";
+
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
 
+
     public void method1(){
          Integer a =     10;
+        Integer b =     10;
+        Integer c =     a + b;
     }
     public void method2(){
         Integer a =     10;
+        Integer b =     10;
+        Integer c =     a + b;
     }
     public void method3(){
         Integer a =     10;
+        Integer b =     10;
+        Integer c =     a + b;
     }
     public void method4(){
         Integer a =     10;
+        Integer b =     10;
+        Integer c =     a + b;
     }
 }
