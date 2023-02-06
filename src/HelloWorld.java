11b053f7e60b795b73e1929f24a10874e8ddd3cf
Darshan.N <darshan.n@clarivate.com>
2023-02-06 18:58:54 +0530

    Smell test of repo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,17 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    public void method1(){
+         Integer a =     10;
+    }
+    public void method2(){
+        Integer a =     10;
+    }
+    public void method3(){
+        Integer a =     10;
+    }
+    public void method4(){
+        Integer a =     10;
+    }
 }
